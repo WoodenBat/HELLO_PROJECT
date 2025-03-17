529483583c9cdb24ceef124d66655153fd8f7c0a
user <user@DESKTOP-AHNPB44>
2025-03-17 14:42:38 +0900

    sadf

--- src/a/Hello.java
+++ src/a/Hello.java
@@ -2,8 +2,6 @@ package a;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println(312312);
-		System.out.println(252525);
-		System.out.println(235255);
+		System.out.println("Hello world");
 	}
 }
