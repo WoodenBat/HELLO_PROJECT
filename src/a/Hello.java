3f29d9df7715948799c2a346f19f0e0f484ed514
WoodenBat <jhs9714@naver.com>
2025-03-17 14:45:26 +0900

    sadf

--- src/a/Hello.java
+++ src/a/Hello.java
@@ -2,7 +2,6 @@ package a;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println(312312);
-		System.out.println(25252225);
+		System.out.println("Hello world");
 	}
 }
