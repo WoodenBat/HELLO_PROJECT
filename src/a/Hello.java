c93a169498da790cccd699f74d651f8f478320ea
WoodenBat <jhs9714@naver.com>
2025-03-17 17:03:51 +0900

    asdfasfd

--- src/a/Hello.java
+++ src/a/Hello.java
@@ -2,7 +2,25 @@ package a;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println(12123);
+		
+		int i = 10;
+		int j = 123123;
+		char ch = 'a';
+		String str = "Hello";
+		float f = 10.021f;
+		boolean bool = true;
+		
+		System.out.println(Integer.toString(i) + 10);
+		System.out.println((Integer.toString(i) + 10).getClass());
+		System.out.println(i + 10);
+		System.out.println(((Object)(i + 10)).getClass());
+		System.out.println(String.valueOf(j) + 10);
+		System.out.println(j + 10);
+		System.out.println((int)ch);
+		
+		if(i > 5) {
+			bool = !bool;
+		}
+		System.out.println(bool);
 	}
 }
